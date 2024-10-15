99fa249a3f972fae3eb05aab078974c1f0a25065
Revivegodx <79395130+Revivegodx@users.noreply.github.com>
2024-10-15 22:38:29 +0700

    Add files via upload

---
+++ find.ps1
@@ -0,0 +1,14 @@
+$a = Get-ChildItem "C:\" -Recurse -Directory -ErrorAction SilentlyContinue
+$a | ForEach-Object {
+    $dirName = $_.FullName
+    $acls = Get-Acl $dirName -ErrorAction SilentlyContinue | Select -ExpandProperty Access | Where-Object {
+        $_.FileSystemRights -match "Write" -and $_.IdentityReference -match "authenticated users|everyone|$env:username"
+    }
+    if ($acls -ne $null) {
+        [pscustomobject]@{
+            DirectoryName = $dirName
+            Users         = $acls | Select -ExpandProperty IdentityReference
+        }
+    }
+}
+$a | Format-Table -AutoSize
